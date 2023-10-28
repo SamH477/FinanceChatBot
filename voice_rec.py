0c23637d5edca6a8997beeb2c8a5ba850941cbb8
SamH477 <huhns@etown.edu>
2023-10-28 16:00:13 -0400

    Add voice recognition as well as a simple UI with a start button

--- voice_rec.py
+++ voice_rec.py
@@ -1,15 +1,28 @@
+from flask import Flask, request, jsonify, render_template
 import speech_recognition as sr
 
-def recognize_cmd(timeout=10):
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-    with microphone as source:
-        try:
-            recognizer.adjust_for_ambient_noise(source) #helps filter out background noise
-            print("Listening...")
-            audio = recognizer.listen(source, timeout=timeout)
-            command = recognizer.recognize_google(audio).lower()
-        except:
-            sr.UnknownValueError
-            return "I'm sorry, I could not understand."
-    return command
+app = Flask(__name__)
+
+# Voice recognition function
+def recognize_cmd(audio_data):
+    command = audio_data  # The audio data is directly passed as the command
+    response = "Command not Recognized"
+    for recognized_command in commands:
+        index = commands.index(recognized_command)
+        if recognized_command in command:
+            response = responses[index]
+    return response
+
+commands = ['hello', 'who are you?', 'goodbye']
+responses = ['Hello! How can I help you?', 'I am a finance chatbot!', 'see ya!']
+
+@app.route('/', methods=['POST', 'GET'])
+def recognition():
+    if request.method == 'POST':
+        audio_data = request.data  # Access the raw audio data
+        response = recognize_cmd(audio_data.decode('utf-8'))
+        return jsonify({'command': audio_data.decode('utf-8'), 'response': response})
+    return render_template('audio_capture.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
